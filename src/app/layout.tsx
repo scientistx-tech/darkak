7866159e07a9f08a9667b23cc6a858033e5c7764
sazzad9911 <sazzad15-2521@diu.edu.bd>
2025-03-17 21:34:33 +0600

    intitial comit

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import "@/css/satoshi.css";
+import "@/css/style.css";
+import "flatpickr/dist/flatpickr.min.css";
+import "jsvectormap/dist/jsvectormap.css";
+import type { Metadata } from "next";
+import { PropsWithChildren } from "react";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | NextAdmin - Next.js Dashboard Kit",
+    default: "NextAdmin - Next.js Dashboard Kit",
+  },
+  description:
+    "Next.js admin dashboard toolkit with 200+ templates, UI components, and integrations for fast dashboard development.",
+};
+
+export default function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        
+                {children}
+             
+      </body>
+    </html>
+  );
+}
