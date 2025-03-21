d2c03d86ab566b0186cdb00718e954302c73e7cf
sazzad9911 <sazzad15-2521@diu.edu.bd>
2025-03-21 14:19:06 +0600

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
         current: "currentColor",
         transparent: "transparent",
         white: "#FFFFFF",
-        primary: "#5750F1",
+        primary: "#F9C726",
         stroke: "#E6EBF1",
         "stroke-dark": "#27303E",
         dark: {
