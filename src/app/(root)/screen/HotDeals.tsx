e02091e6322f72ef6e2ab3e876f7b5d87c221711
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2025-03-22 04:00:00 +0600

    Card add

--- src/app/(root)/screen/HotDeals.tsx
+++ src/app/(root)/screen/HotDeals.tsx
@@ -1,9 +1,42 @@
-"use client";
-
+import HotCard from "@/components/shared/HotCard";
 import React from "react";
 
+import Img1 from "@/Data/Demo/iPad-Air-M3---2025-1259.jpg";
+import Img2 from "@/Data/Demo/iPad-Air-M3---2025-purple-1209.jpg";
+import Img3 from "@/Data/Demo/iPad-Air-M3---2025-space-gray-3684.jpg";
+import Img4 from "@/Data/Demo/iPad-Air-M3---2025-starlight-2313.jpg";
+
 const HotDeals: React.FC = () => {
-  return <div className="text-center text-2xl font-bold">Hot Deals</div>;
+  return (
+    <div className="w-full">
+      <p className="mt-10 text-2xl font-semibold uppercase text-primary md:mt-16 md:text-3xl">
+        Hot <samp className="text-secondary">Deals</samp>
+      </p>
+
+      <div className="mt-10">
+        <HotCard
+          name="iPad Air M3 - 2025"
+          regularPrice={80200}
+          discount={20}
+          status="In Stock"
+          description="Go beyond during every creative project with Apple iPad Air M3. One of the slimmest designs with premium colors looks fashionable and aesthetic at the same time. Get gorgeous visuals from the large 11-inch or 13-inch model, which has Wide Color (P3) supported for immersive viewing."
+          mainImage={Img1}
+          image1={Img2}
+          image2={Img3}
+          image3={Img4}
+          rating={3.5}
+          reviews={10}
+          cartLink=""
+          buyLink=""
+          favoriteLink=""
+        />
+
+        {/* <HotCard />
+
+        <HotCard /> */}
+      </div>
+    </div>
+  );
 };
 
 export default HotDeals;
--- src/components/shared/HotCard.tsx
+++ src/components/shared/HotCard.tsx
@@ -1,7 +1,96 @@
-import React from 'react'
+"use client";
 
-export default function HotCard() {
-  return (
-    <div>HotCard</div>
-  )
+import React, { useState, useEffect } from "react";
+import Image, { StaticImageData } from "next/image";
+
+import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
+import Link from "next/link";
+
+interface HotCardProps {
+  name: string;
+  regularPrice: number;
+  discount: number;
+  status: string;
+  description: string;
+  mainImage: StaticImageData;
+  image1: StaticImageData;
+  image2: StaticImageData;
+  image3: StaticImageData;
+  rating: number;
+  reviews: number;
+  cartLink: string;
+  buyLink: string;
+  favoriteLink: string;
 }
+
+const HotCard: React.FC<HotCardProps> = ({
+  name,
+  regularPrice,
+  discount,
+  status,
+  description,
+  mainImage,
+  image1,
+  image2,
+  image3,
+  rating,
+  reviews,
+  cartLink,
+  buyLink,
+  favoriteLink,
+}) => {
+  const newPrice = regularPrice - (regularPrice * discount) / 100;
+  const renderStars = () => {
+    const fullStars = Math.floor(rating);
+    const halfStar = rating % 1 !== 0;
+    const emptyStars = 5 - fullStars - (halfStar ? 1 : 0);
+
+    return (
+      <div className="flex items-center text-yellow-500">
+        {[...Array(fullStars)].map((_, i) => (
+          <FaStar key={i} />
+        ))}
+        {halfStar && <FaStarHalfAlt />}
+        {[...Array(emptyStars)].map((_, i) => (
+          <FaRegStar key={i} />
+        ))}
+      </div>
+    );
+  };
+
+  const shortDescription =
+    description.split(" ").slice(0, 20).join(" ") +
+    (description.split(" ").length > 20 ? "..." : "");
+
+  return (
+    <div className="rounded-md bg-white p-5">
+      <div className="flex">
+        <div>
+          <Image alt="img" src={mainImage} className="h-[200px] w-[200px]"/>
+        </div>
+
+        <div>
+          <p className="text-[12px] text-primary line-through">
+            Regular Price: {regularPrice}
+          </p>
+          <p className="font-semibold text-green-600">
+            Price: {newPrice.toFixed(2)}
+          </p>
+          <p className="text-xl text-secondary">{name}</p>
+          <p>{shortDescription}</p>
+
+          <p>Raring: ({reviews} reviews)</p>
+          <div className="flex items-center gap-1">{renderStars()}</div>
+        </div>
+      </div>
+
+      <div className="flex justify-between">
+        <Link href={buyLink}  className="text-xl font-medium rounded-full border border-primary bg-transparent p-2 pl-4 pr-4 text-secondary  hover:bg-primary">Buy Now</Link>
+
+        <Link href={cartLink}  className="text-xl font-medium rounded-full border border-primary bg-primary p-2 pl-4 pr-4 text-secondary hover:bg-transparent hover:text-primary">Add to Cart</Link>
+      </div>
+    </div>
+  );
+};
+
+export default HotCard;
