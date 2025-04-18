6da88d0cc6ffd323424dc12014f4df96d0d792d9
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2025-04-19 00:07:36 +0600

    ok

---
+++ src/app/(root)/category/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CategoryPage from './CategoryPage'
+
+export default function page() {
+  return (
+    <div className='w-full'>
+        <CategoryPage />
+    </div>
+  )
+}
