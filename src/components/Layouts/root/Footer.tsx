8667cde854c37303823769eea91c6057d20394fb
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2025-04-18 16:57:33 +0600

    add

--- src/components/Layouts/root/Footer.tsx
+++ src/components/Layouts/root/Footer.tsx
@@ -25,7 +25,8 @@ function Footer() {
           src={imgBg}
           className="absolute h-full w-full object-cover opacity-10"
         />
-        <div className="absolute w-full flex">
+        <div className="absolute flex w-full">
+          {/* Left-side */}
           <div className="w-2/3 pl-5 md:pl-10">
             <div className="mt-[-65px] flex w-[50%] flex-col items-center justify-center rounded-md bg-[#F1F6FF] px-3 py-6 shadow-2xl">
               <p className="text-xl font-semibold text-[#003084]">
@@ -43,22 +44,123 @@ function Footer() {
               </div>
             </div>
 
-            <div className="w-full mt-[70px] flex">
-              <div className="flex w-2/3">
-                <div className="w-1/2 flex flex-col">
-                  <p className="mb-5 text-xl text-[#BBD4FF]">Quick Links</p>
+            <div className="mt-[70px] flex w-full">
+              <div className="flex w-2/3 flex-col">
+                <div className="flex w-full">
+                  <div className="flex w-1/2 flex-col">
+                    <p className="mb-5 text-xl text-[#BBD4FF]">Quick Links</p>
 
-                  <Link href="/" className="text-[#F6F6F6] hover:text-white">Home</Link>
-                  <Link href="/shop" className="mt-3 text-[#F6F6F6] hover:text-white">Shop</Link>
-                  <Link href="/product" className="mt-3 text-[#F6F6F6] hover:text-white">Product</Link>
-                  <Link href="/contact-us" className="mt-3 text-[#F6F6F6] hover:text-white">Contact Us</Link>
+                    <Link href="/" className="text-[#F6F6F6] hover:text-white">
+                      Home
+                    </Link>
+                    <Link
+                      href="/shop"
+                      className="mt-3 text-[#F6F6F6] hover:text-white"
+                    >
+                      Shop
+                    </Link>
+                    <Link
+                      href="/product"
+                      className="mt-3 text-[#F6F6F6] hover:text-white"
+                    >
+                      Product
+                    </Link>
+                    <Link
+                      href="/contact-us"
+                      className="mt-3 text-[#F6F6F6] hover:text-white"
+                    >
+                      Contact Us
+                    </Link>
+                  </div>
+                  <div className="flex w-1/2 flex-col">
+                    <p className="mb-5 text-xl text-[#BBD4FF]">Support</p>
+
+                    <Link
+                      href="/about-us"
+                      className="text-[#F6F6F6] hover:text-white"
+                    >
+                      About Us
+                    </Link>
+                    <Link
+                      href="/privacy-policy"
+                      className="mt-3 text-[#F6F6F6] hover:text-white"
+                    >
+                      Privacy policy
+                    </Link>
+                    <Link
+                      href="/terms-and-condition"
+                      className="mt-3 text-[#F6F6F6] hover:text-white"
+                    >
+                      Terms and Condition
+                    </Link>
+                    <Link
+                      href="/faq"
+                      className="mt-3 text-[#F6F6F6] hover:text-white"
+                    >
+                      FAQ
+                    </Link>
+                  </div>
                 </div>
-                <div className="w-1/2"></div>
+
+                <p className="mt-8 text-2xl text-[#BBD4FF]">
+                  Download Our Mobile App
+                </p>
+                <div className="mt-5 flex justify-start gap-5">
+                  <button className="h-[50px] w-auto">
+                    <Image src={img1} alt="" className="h-full w-full" />
+                  </button>
+
+                  <button className="h-[50px] w-auto">
+                    <Image src={img2} alt="" className="h-full w-full" />
+                  </button>
+                </div>
               </div>
-              <div className="w-1/3"></div>
+              <div className="flex w-1/3 flex-col">
+                <p className="mb-5 text-xl text-[#BBD4FF]">
+                  Product Categories
+                </p>
+
+                <Link
+                  href="/categories/"
+                  className="text-[#F6F6F6] hover:text-white"
+                >
+                  Electronics
+                </Link>
+                <Link
+                  href="/categories/"
+                  className="mt-3 text-[#F6F6F6] hover:text-white"
+                >
+                  Clothing
+                </Link>
+                <Link
+                  href="/categories/"
+                  className="mt-3 text-[#F6F6F6] hover:text-white"
+                >
+                  Groceries
+                </Link>
+                <Link
+                  href="/categories/"
+                  className="mt-3 text-[#F6F6F6] hover:text-white"
+                >
+                  Accessories
+                </Link>
+                <Link
+                  href="/categories/"
+                  className="mt-3 text-[#F6F6F6] hover:text-white"
+                >
+                  Shoes
+                </Link>
+                <Link
+                  href="/categories/"
+                  className="mt-3 text-[#F6F6F6] hover:text-white"
+                >
+                  Books
+                </Link>
+              </div>
             </div>
           </div>
 
+          {/* Right-side */}
           <div className="w-1/3">Left</div>
         </div>
       </div>
