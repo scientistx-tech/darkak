63504cea5c01ca66db820e000c7cbd58f545a4c2
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2025-04-25 23:50:23 +0600

    update section

--- src/app/(root)/screen/BestSelling.tsx
+++ src/app/(root)/screen/BestSelling.tsx
@@ -4,8 +4,7 @@ import React from "react";
 import ProductCard from "@/components/shared/ProductCard";
 import { Product } from "../types/ProductType";
 import Image from "next/image";
-
-import images from "@/Data/Demo/Rectangle 130 (1).png";
+import laptop from "@/Data/Demo/Rectangle 130 (1).png";
 
 const dummyProducts: Product[] = new Array(7).fill(null).map((_, i) => ({
   id: `prod-${i}`,
@@ -18,47 +17,43 @@ const dummyProducts: Product[] = new Array(7).fill(null).map((_, i) => ({
   price: 800,
   originalPrice: 1000,
   storage: "12GB/512GB",
-  discount: 20,
+  discount: 10,
   rating: 4.5,
   reviews: 65,
 }));
 
 const BestSelling: React.FC = () => {
   return (
-    <section className="mt-10">
-      <div className="mb-6 flex items-center justify-between">
-        <h2 className="xl:ml-[26%] text-2xl font-bold text-primaryDarkBlue">
+    <section className="mt-10 px-2 container mx-auto">
+      <div className="mb-1 flex items-center justify-between">
+        <h2 className="text-2xl lg:ml-[25%] xl:ml-[20%] md:ml-[33%] font-semibold  text-primaryDarkBlue">
           BEST SELLING PRODUCTS
         </h2>
         <span className="cursor-pointer text-2xl">→</span>
       </div>
 
-      <div className="w-full">
-        {/* LEFT BANNER */}
-
-        {/* PRODUCTS GRID */}
-        <div className="grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 lg:gap-6 xl:gap-8">
-          <div className="hidden xl:block">
-            <div className="absolute mt-[-50px] flex flex-col justify-between rounded-lg bg-blue-600 p-5 text-white">
-              <div className="mt-6">
-                <h3 className="mb-2 text-sm font-semibold">SUMMER OFFER</h3>
-                <p className="mt-5 text-2xl font-medium leading-tight">
-                  GET 10% DISCOUNT ON <br /> FIRST PURCHASE
-                </p>
-              </div>
-              <div className="mt-16 mb-6 flex w-full justify-center">
-                <Image
-                  src={images}
-                  alt="Offer Laptop"
-                  className="h-[200px] w-auto object-contain"
-                />
-              </div>
-            </div>
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:gap-8 relative">
+        {/* LEFT SIDE BANNER */}
+        <div className="absulate hidden md:h-[425px] lg:h-[420px] xl:h-[450px] rounded-xl md:mt-[-45px] lg:mt-[-40px] xl:mt-[-40px] 2xl:mt-[-50px] bg-[#4C84FF] p-6 text-white md:flex flex-col justify-between ">
+          <div>
+            <h3 className="text-sm font-semibold mb-3">SUMMER OFFER</h3>
+            <p className="text-2xl font-semibold leading-tight">
+              GET 10% DISCOUNT ON <br /> FIRST PURCHASE
+            </p>
           </div>
-          {dummyProducts.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
+          <div className="mt-auto pt-8 flex justify-center">
+            <Image
+              src={laptop}
+              alt="Offer Laptop"
+              className="w-[200px] object-contain"
+            />
+          </div>
         </div>
+
+        {/* PRODUCT CARDS */}
+        {dummyProducts.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
       </div>
     </section>
   );
