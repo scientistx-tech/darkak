431dd6be32b0406575d259c29a315cf65131442e
Emon <150001421+doodleEmon@users.noreply.github.com>
2025-04-27 16:42:36 +0600

    workin on category page

---
+++ src/app/(root)/category/components/SortBy.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SortBy = () => {
+  return (
+    <div className='bg-[#5694FF]'>
+      <div>
+        <p className='text-3xl font-semibold'>Phone & accessories</p>
+      </div>
+      <div>
+        <p>Sort By</p>
+        <div>options</div>
+      </div>
+    </div>
+  )
+}
+
+export default SortBy;
