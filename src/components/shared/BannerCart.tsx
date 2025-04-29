6e65231557b17607c5c43ed713acc1b054ffd126
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2025-04-29 23:25:54 +0600

    ok

--- src/components/shared/BannerCart.tsx
+++ src/components/shared/BannerCart.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 import clsx from "clsx";
+import ShopNowButton from "@/app/(root)/profile/components/ShopNowButton";
 
 interface BannerCardProps {
   bgColour: string;
@@ -44,31 +45,7 @@ const BannerCart: React.FC<BannerCardProps> = ({
         </p>
         <div className="h-[10px] w-full"></div>
 
-        <Link
-          href={link}
-          className="group relative inline-flex items-center justify-center overflow-hidden rounded-full border-2 border-primary bg-primary p-4 px-6 py-2 font-medium text-indigo-600 shadow-md transition duration-300 ease-out"
-        >
-          <span className="ease absolute inset-0 flex h-full w-full -translate-x-full items-center justify-center bg-primary text-white duration-300 group-hover:translate-x-0">
-            <svg
-              className="h-6 w-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M14 5l7 7m0 0l-7 7m7-7H3"
-              ></path>
-            </svg>
-          </span>
-          <span className="ease absolute flex h-full w-full transform items-center justify-center text-white transition-all duration-300 group-hover:translate-x-full">
-            Shop Now
-          </span>
-          <span className="invisible relative">Shop Now</span>
-        </Link>
+        <ShopNowButton link={link} text="Shop Now" />
       </div>
 
       <div className="w-[40%]">
