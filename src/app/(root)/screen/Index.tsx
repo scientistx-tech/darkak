4066bf8615b9e283793f74949870dffed3a6a267
Anikcse19 <anikdebcse@gmail.com>
2025-05-21 23:44:29 +0600

    checkout page

--- src/app/(root)/screen/Index.tsx
+++ src/app/(root)/screen/Index.tsx
@@ -24,7 +24,7 @@ const slides = [
     title: "LIMITED TIME OFFER",
   },
   {
-    baner: img3,
+    baner: img1,
     offer_name: "Tech that Empowers You",
     details: "Explore innovation with every click",
     ttile: "EXCLUSIVE LAUNCH",
