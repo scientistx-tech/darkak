b826113c3f8f2c4e34f03cab1d02c117b1dcd874
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2025-05-25 04:24:25 +0600

    footer And header mega menu

--- src/components/Layouts/root/FooterNav.tsx
+++ src/components/Layouts/root/FooterNav.tsx
@@ -6,13 +6,22 @@ import React from "react";
 import {
   FaHome,
   FaShoppingBag,
-  FaBox,
-  FaBlog,
+  FaShoppingCart,
   FaEnvelope,
+  FaHeart,
 } from "react-icons/fa";
+
+import { useGetMyCartQuery } from "@/redux/services/client/myCart";
+import { useGetMyWishListQuery } from "@/redux/services/client/myWishList";
 
 const FooterNav: React.FC = () => {
   const pathname = usePathname();
+
+  const { data: cart, refetch: cartRefetch } = useGetMyCartQuery();
+  const { data: wishlist, refetch: wishRefetch } = useGetMyWishListQuery({
+    page: 1,
+    limit: 100,
+  });
 
   return (
     <div className="fixed bottom-0 left-0 z-50 flex h-[60px] w-full items-center justify-evenly bg-[#00286EF2] text-white">
@@ -32,27 +41,45 @@ const FooterNav: React.FC = () => {
       >
         <FaShoppingBag
           size={24}
-          className={`${pathname === "/shop" ? "text-primary" : "text-white"} transition-all duration-300 hover:text-primary`}
+          className={`${pathname === "/category" ? "text-primary" : "text-white"} transition-all duration-300 hover:text-primary`}
         />
       </Link>
 
       <Link
-        href="/category"
+        href="/user/wishlist"
         className="flex flex-col items-center transition-all duration-300"
       >
-        <FaBox
+        <div
+          className={`absolute ml-[25px] mt-[-8px] flex h-[15px] w-[15px] items-center justify-center rounded-full text-black group-hover:bg-primary group-hover:text-white ${
+            pathname === "/user/wishlist" ? "bg-white" : "bg-primary text-white"
+          }`}
+        >
+          <p className="text-[10px] font-semibold">
+            {wishlist ? wishlist.data.length : "0"}
+          </p>
+        </div>
+        <FaHeart
           size={24}
-          className={`${pathname === "/product" ? "text-primary" : "text-white"} transition-all duration-300 hover:text-primary`}
+          className={`${pathname === "/user/wishlist" ? "text-primary" : "text-white"} transition-all duration-300 hover:text-primary`}
         />
       </Link>
 
       <Link
-        href="/blogs"
+        href="/user/cart"
         className="flex flex-col items-center transition-all duration-300"
       >
-        <FaBlog
+        <div
+          className={`absolute ml-[25px] mt-[-8px] flex h-[15px] w-[15px] items-center justify-center rounded-full text-black group-hover:bg-primary group-hover:text-white ${
+            pathname === "/user/cart" ? "bg-white" : "bg-primary text-white"
+          }`}
+        >
+          <p className="text-[10px] font-semibold">
+            {cart ? cart.cart.length : "0"}
+          </p>
+        </div>
+        <FaShoppingCart
           size={24}
-          className={`${pathname === "/blogs" ? "text-primary" : "text-white"} transition-all duration-300 hover:text-primary`}
+          className={`${pathname === "/user/cart" ? "text-primary" : "text-white"} transition-all duration-300 hover:text-primary`}
         />
       </Link>
 
