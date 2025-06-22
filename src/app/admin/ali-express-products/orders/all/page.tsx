551652d94ab09faa0eddc1d9c6033a678d01668f
tafiya <tafiapinkey@email.com>
2025-06-22 17:44:53 +0600

    fixing dark mode color in admin dashboard

--- src/app/admin/ali-express-products/orders/all/page.tsx
+++ src/app/admin/ali-express-products/orders/all/page.tsx
@@ -85,7 +85,7 @@ const AllOrderList = () => {
                   onChange={(e) => {
                     setSearch(e.target.value);
                   }}
-                  className="w-full rounded-md border border-gray-300 px-4 py-2 pl-10 text-sm outline-none focus:outline-none dark:bg-gray-500"
+                  className="w-full rounded-md border dark:bg-gray-700 border-gray-300 px-4 py-2 pl-10 text-sm outline-none focus:outline-none dark:bg-gray-500"
                 />
                 <span className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400">
                   <svg
