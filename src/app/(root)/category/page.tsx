e81714c303d838987555394744d0342f118b8402
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2025-07-05 21:44:12 +0600

    seo update

--- src/app/(root)/category/page.tsx
+++ src/app/(root)/category/page.tsx
@@ -1,6 +1,29 @@
 import React from 'react'
 import CategoryPageServer from './CategoryPageServer'
 
+import getSeoData from '../getSeoData';
+export async function generateMetadata() {
+  const data = await getSeoData('category');
+
+  return {
+    title: data?.data?.meta_title || '',
+    description: data?.data?.meta_description || '',
+    keywords: data?.data?.meta_keywords?.map((d: any) => d) || [],
+    openGraph: {
+      title: data?.data?.meta_title || '',
+      description: data?.data?.meta_description || '',
+      images: [
+        {
+          url: data?.data?.meta_image, // Update with your image URL
+          width: 1200,
+          height: 630,
+          alt: data?.data?.meta_alt,
+        },
+      ],
+    },
+  };
+}
+
 export default function page() {
   return (
     <div>
