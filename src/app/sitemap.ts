d312ebf29066097278f1f50e17a835e18d3c7605
sazzad9911 <sazzad15-2521@diu.edu.bd>
2025-08-01 23:55:59 +0600

    asdasd

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,18 +1,37 @@
-import type { MetadataRoute } from "next";
+import { MetadataRoute } from 'next';
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: "https://darkak.com.bd",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: "https://darkak.com.bd/auth/login",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-  ];
+type RefreshType = 'weekly' | 'monthly' | 'yearly';
+
+interface Sitemap {
+  id: number;
+  title: string;
+  url: string;
+  priority: number;
+  date: string; // or Date if you parse it
+  refresh: RefreshType;
 }
+
+// export async function generateSitemaps() {
+//   // Fetch the total number of products and calculate the number of sitemaps needed
+//   return [{ id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }];
+// }
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  // Google's limit is 50,000 URLs per sitemap
+  const maps = await getSiteMap();
+  return maps.map((data: Sitemap) => ({
+    url: data.url,
+    lastModified: data.date,
+    changeFrequency: data.refresh,
+    priority: data.priority,
+  }));
+}
+const getSiteMap = async () => {
+  const res = await fetch(`https://api.darkak.com.bd/api/public/sitemap`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return res.json();
+};
